60cb5f2b816162730fa07ebc719fa8aa65cef87c
Mitch Lewsey <mitch.lewsey@gmail.com>
2026-06-03 12:21:00 +0100

    tdd'd BoardGame class, all tests passing

---
+++ lib/board_game.py
@@ -0,0 +1,28 @@
+from lib.db import db
+
+
+class BoardGame(db.Model):
+    __tablename__ = 'board_game'
+
+    id           = db.Column(db.Integer,     primary_key=True)
+    name         = db.Column(db.String(255), nullable=False)
+    bgg_id       = db.Column(db.Integer)
+    factory_upc  = db.Column(db.String(20),  unique=True)
+    min_players  = db.Column(db.SmallInteger)
+    max_players  = db.Column(db.SmallInteger)
+    min_time     = db.Column(db.SmallInteger)
+    max_time     = db.Column(db.SmallInteger)
+    publisher    = db.Column(db.String(255))
+    designer     = db.Column(db.String(255))
+    artist       = db.Column(db.String(255))
+    is_expansion = db.Column(db.Boolean,     nullable=False, default=False)
+    base_game_id = db.Column(db.Integer,     db.ForeignKey('board_game.id', ondelete='SET NULL'))
+    avg_rating   = db.Column(db.Numeric(4, 2))
+
+    def __eq__(self, other):
+        game1 = {k: v for k, v in self.__dict__.items() if k != '_sa_instance_state'}
+        game2 = {k: v for k, v in other.__dict__.items() if k != '_sa_instance_state'}
+        return game1 == game2
+    
+    def __repr__(self):
+        return f"BoardGame({self.id}, {self.name}, {self.bgg_id}, {self.factory_upc}, {self.min_players}, {self.max_players}, {self.min_time}, {self.max_time}, {self.publisher}, {self.designer}, {self.artist}, {self.is_expansion}, {self.base_game_id}, {self.avg_rating})"
--- tests/test_board_game_model.py
+++ tests/test_board_game_model.py
@@ -1,9 +1,12 @@
 from lib.board_game import BoardGame
-from decimal import Decimal
 
+"""
+Board Game constructs with all related attributes
+"""
 
-def test_board_game_has_correct_attributes(db):
+def test_board_game_constructs():
     game = BoardGame(
+        id=1,
         name="Pinched!",
         bgg_id=450685,
         factory_upc="012345678901",
@@ -15,21 +18,75 @@ def test_board_game_has_correct_attributes(db):
         designer="Jonathan Gilmour-Long, David Gordon (I)",
         artist="Max Kosek, Vesna 'vesner' Redesiuk",
         is_expansion=False,
+        base_game_id=None,
         avg_rating=7.15,
     )
-    db.session.add(game)
-    db.session.commit()
 
-    saved = db.session.get(BoardGame, game.id)
+    assert game.name == "Pinched!"
+    assert game.bgg_id == 450685
+    assert game.min_players == 2
+    assert game.max_players == 5
+    assert game.min_time == 45
+    assert game.max_time == 60
+    assert game.publisher == "Mighty Boards, Lucky Duck Games"
+    assert game.designer == "Jonathan Gilmour-Long, David Gordon (I)"
+    assert game.artist == "Max Kosek, Vesna 'vesner' Redesiuk"
+    assert game.is_expansion is False
+    assert game.base_game_id is None
+    assert game.avg_rating == 7.15
 
-    assert saved.name == "Pinched!"
-    assert saved.bgg_id == 450685
-    assert saved.min_players == 2
-    assert saved.max_players == 5
-    assert saved.min_time == 45
-    assert saved.max_time == 60
-    assert saved.publisher == "Mighty Boards, Lucky Duck Games"
-    assert saved.designer == "Jonathan Gilmour-Long, David Gordon (I)"
-    assert saved.artist == "Max Kosek, Vesna 'vesner' Redesiuk"
-    assert saved.is_expansion is False
-    assert float(saved.avg_rating) == 7.15
+def test_two_board_games_are_equal():
+    game_1 = BoardGame(
+        id=1,
+        name="Pinched!",
+        bgg_id=450685,
+        factory_upc="012345678901",
+        min_players=2,
+        max_players=5,
+        min_time=45,
+        max_time=60,
+        publisher="Mighty Boards, Lucky Duck Games",
+        designer="Jonathan Gilmour-Long, David Gordon (I)",
+        artist="Max Kosek, Vesna 'vesner' Redesiuk",
+        is_expansion=False,
+        base_game_id=None,
+        avg_rating=7.15,
+    )
+
+    game_2 = BoardGame(
+        id=1,
+        name="Pinched!",
+        bgg_id=450685,
+        factory_upc="012345678901",
+        min_players=2,
+        max_players=5,
+        min_time=45,
+        max_time=60,
+        publisher="Mighty Boards, Lucky Duck Games",
+        designer="Jonathan Gilmour-Long, David Gordon (I)",
+        artist="Max Kosek, Vesna 'vesner' Redesiuk",
+        is_expansion=False,
+        base_game_id=None,
+        avg_rating=7.15,
+    )
+
+    assert game_1 == game_2
+
+def test_stringify_board_game():
+    game_1 = BoardGame(
+        id=1,
+        name="Pinched!",
+        bgg_id=450685,
+        factory_upc="012345678901",
+        min_players=2,
+        max_players=5,
+        min_time=45,
+        max_time=60,
+        publisher="Mighty Boards, Lucky Duck Games",
+        designer="Jonathan Gilmour-Long, David Gordon (I)",
+        artist="Max Kosek, Vesna 'vesner' Redesiuk",
+        is_expansion=False,
+        base_game_id=None,
+        avg_rating=7.15,
+    )
+    assert str(game_1) == "BoardGame(1, Pinched!, 450685, 012345678901, 2, 5, 45, 60, Mighty Boards, Lucky Duck Games, Jonathan Gilmour-Long, David Gordon (I), Max Kosek, Vesna 'vesner' Redesiuk, False, None, 7.15)"
